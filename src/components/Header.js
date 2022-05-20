d09a940f498f2b2ae767801ce17193d7eb9d7dd3
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-05-20 15:38:51 +0800

    Added UI logic to change depending if user is logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,33 @@
+import { useState } from 'react';
+
 import "../css/Header.css";
 
 export default function Header() {
 
+    //const [username, setUsername] = useState(null);
+    const [username, setUsername] = useState("Joe"); // TODO
+
     return (
         <section className="header">
-            <nav>
 
-                <a href="/">
-                    <img src="img/icon.png" alt="Pathfinder" />
-                </a>
+            <a href="/">
+                <img src="img/icon.png" alt="Pathfinder" height="100" width="100"/>
+            </a>
 
-                <div className="nav-links">
+            <div className="nav-links">
+                {username == null ? (
                     <ul>
-                        <li><a href="/Login"> Login</a></li>
-                        <li><a href="/SignUp"> Sign Up</a></li>
+                        <li><a href="/SignUp">Sign Up</a></li>
+                        <li><a href="/Login">Login</a></li>
                     </ul>
-                </div>
+                ) : (
+                    <ul>
+                        <li>Welcome, {username}!</li>
+                        <li><a href="/Logout">Logout</a></li>
+                    </ul>
+                )}
+            </div>
 
-            </nav>
         </section>
     );
 }
