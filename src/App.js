8fc8a8317d3bcad5ee4347e5562c184d4d4c857a
LAPTOP-PE4TRUP8\Kristen <kristenchung4@gmail.com>
2022-06-03 23:26:04 +0800

    Created route for reset password

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Forget_password from "./pages/Forget_password"
+import ResetPassword from "./pages/ResetPassword"
 
 import "./css/styles.css";
 
@@ -14,6 +15,7 @@ export default function App() {
                     <Route path = "/" element = {<Home/>}/>
                     <Route path = "/sign-up" element = {<SignUp/>}/>
                     <Route path = "/forget_password" element = {<Forget_password/>}/>
+                    <Route path ="/reset-password"element = {<ResetPassword/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
