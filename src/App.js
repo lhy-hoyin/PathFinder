0880ce179e994ea4fede5b984bb072734e491b16
LAPTOP-PE4TRUP8\Kristen <kristenchung4@gmail.com>
2022-06-07 12:53:15 +0800

    Intergrated the protected route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import ProtectedRoute from './components/ProtectedRoute'
+
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Forget_password from "./pages/Forget_password"
@@ -15,7 +17,12 @@ export default function App() {
                     <Route path = "/" element = {<Home/>}/>
                     <Route path = "/sign-up" element = {<SignUp/>}/>
                     <Route path = "/forget_password" element = {<Forget_password/>}/>
-                    <Route path ="/reset-password"element = {<ResetPassword/>}/>
+
+                    //any route related to the user enter here eg. profile, reset password, etc
+                    <Route path='/' element = {<ProtectedRoute/>}>
+                        <Route path ="/reset-password"element = {<ResetPassword/>}/>
+                    </Route>
+
                 </Routes>
             </BrowserRouter>
         </div>
