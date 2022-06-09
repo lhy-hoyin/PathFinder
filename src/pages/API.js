8eaa125fbe0b534fd5ffc3266ef95f0f144f1324
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-06-09 15:49:08 +0800

    Improved API pulling

--- src/pages/API.js
+++ src/pages/API.js
@@ -8,7 +8,7 @@ export default function API() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [data, setData] = useState([]);
 
-    var url = API_BASE_URL + "2021-2022/modules/CS2030S.json"
+    var url = API_BASE_URL + "2021-2022/modules/CS2106.json"
 
     useEffect(() => {
         fetch(url)
@@ -26,17 +26,36 @@ export default function API() {
             )
     }, [])
 
+    function replacer(key, value) {
+        switch (key) {
+            // filter out unwanted info
+            case "description":
+            case "attributes":
+            case "semesterData":
+            case "workload":
+            case "timetable":
+                return undefined;
+                break;
+            default:
+                return value;
+        }
+    }
+
     return (
         <>
             <h1>API</h1>
 
-            <h3>Search</h3>
+            <h3>Searching</h3>
+            { url }
             {error ? <div>Error: {error.message}</div> : <></>}
             {isLoaded ? <></> : <div>Loading...</div>}
 
 
             <h3>Results</h3>
-            {JSON.stringify(data)}
+            <textarea
+                disabled
+                style={{ width: "100%" }}
+                value={JSON.stringify(data, replacer, '    ')} />
         </>
     )
 
