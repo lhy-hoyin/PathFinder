b38d08d1b373a74c719023af4636bbe44b898d26
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-06-09 14:08:12 +0800

    Updated ProtectedRoute to support other types of protection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import ProtectedRoute from './components/ProtectedRoute'
+import { UserOnlyRoute } from './components/ProtectedRoute'
 
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
@@ -20,13 +20,13 @@ export default function App() {
                 <Routes>
                     <Route path="/api" element={<API />} />
 
-                    <Route path = "/" element = {<Home/>}/>
-                    <Route path = "/sign-up" element = {<SignUp/>}/>
-                    <Route path = "/forget_password" element = {<Forget_password/>}/>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/sign-up" element={<SignUp/>}/>
+                    <Route path="/forget_password" element={<Forget_password/>}/>
 
                     //any route related to the user enter here eg. profile, reset password, etc
-                    <Route path='/' element = {<ProtectedRoute/>}>
-                        <Route path ="/reset-password"element = {<ResetPassword/>}/>
+                    <Route path='/' element={<UserOnlyRoute/>}>
+                        <Route path="/reset-password"element={<ResetPassword/>}/>
                         <Route path="/profile" element={<UserProfile />} />
                         <Route path="/profile/new" element={<NewUser />} />
                     </Route>
