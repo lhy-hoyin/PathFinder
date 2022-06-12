959a69ac1e31fdb56ea134b45942640b9637b777
LAPTOP-PE4TRUP8\Kristen <kristenchung4@gmail.com>
2022-06-12 19:33:54 +0800

    Added the graph component onto the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 import Header from "../components/Header";
+import Graph from "../components/Graph"
+
+
+
 
 import "../css/Home.css";
 
 export default function Home() {
+    const [course, setCourse] = useState("");
+    
     return (
         <>
             <Header />
@@ -11,6 +19,18 @@ export default function Home() {
                 <p> lol idk only limited to SoC</p>
                 <p> Maybe put a getting started button here as a "tutorial"</p>
             </div>
+            <div>
+                <p>Select Course: </p>
+                <select required onChange={(e) => setCourse(e.target.value)}>
+                    <option>Computer Science</option>
+                    <option>Option2</option>
+                </select> 
+                
+            </div> 
+
+            <Graph/>
+
+            
         </>
   );
 }
