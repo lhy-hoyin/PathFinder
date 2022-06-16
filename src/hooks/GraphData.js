68e36ce5cf08d592d67d63df1bc6944e4895b151
LAPTOP-PE4TRUP8\Kristen <kristenchung4@gmail.com>
2022-06-16 10:04:56 +0800

    integrated backend where nodes can be retrieved

--- src/hooks/GraphData.js
+++ src/hooks/GraphData.js
@@ -15,45 +15,45 @@ export const graphData = () => {
 
 function useProvideGraphData() {
 
-    const getNodes = (setNodes) => async e => {
-      e.preventDefault();
-      let nodes = []
-      try {
-        let { data, error, status, count } = await supabase
-          .from("testing")
-          .select("module", { count: "exact" });
-        if (error && status !== 406) {
-          throw error;
-        }
-    
-        if (data) {
-          console.log(data);
-          let temp = data.slice(0);
-          //console.log(Object.values(temp[1]).toString());
-          //console.log(count);
-          let x = 0;
-    
-          while (x !== count) {
-            nodes[x] = {id: Object.values(temp[x]).toString(),
-                        label: Object.values(temp[x]).toString()};
-            x = x + 1;
-          }
-          console.log(nodes)
-          setNodes(nodes);  
-          nodes = []
-        }
-      } catch (error) {
-        alert(error.message);
+  const getNodes = (setNodes) => async e => {
+    e.preventDefault();
+    let nodes = []
+    try {
+      let { data, error, status, count } = await supabase
+        .from("testing")
+        .select("module", { count: "exact" });
+      if (error && status !== 406) {
+        throw error;
       }
-    };
   
-    const test = (setMessage) => async e =>{
-      e.preventDefault();
-      setMessage("Hellow world")
+      if (data) {
+        console.log(data);
+        let temp = data.slice(0);
+        //console.log(Object.values(temp[1]).toString());
+        //console.log(count);
+        let x = 0;
+  
+        while (x !== count) {
+          nodes[x] = {id: Object.values(temp[x]).toString(),
+                      label: Object.values(temp[x]).toString()};
+          x = x + 1;
+        }
+        console.log(nodes)
+        setNodes(nodes);  
+        nodes = []
+      }
+    } catch (error) {
+      alert(error.message);
     }
-  
-    return {
-        getNodes,
-        test,
-    };
+  };
+
+  const test = (setMessage) => async e =>{
+    e.preventDefault();
+    setMessage("Hellow world")
   }
+
+  return {
+      getNodes,
+      test,
+  };
+}
