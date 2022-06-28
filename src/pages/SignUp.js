7ec2aea3f7de603f88e71ae271c78209bb3ac70d
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-06-28 14:26:34 +0800

    Added Chakra and dependencies

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Input } from '@chakra-ui/react'
 
 import { Auth } from "../hooks/Auth";
 import { supabase } from "../supabaseClient";
