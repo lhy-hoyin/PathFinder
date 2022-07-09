e7edfb812fb7005da673a388182a215172da68f0
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-07-09 12:04:34 +0800

    feat(api wrapper): changed way how moduleExist check for module validity

--- src/hooks/NUSModsAPI.js
+++ src/hooks/NUSModsAPI.js
@@ -1,30 +1,35 @@
 const BASE_URL = "https://api.nusmods.com/v2/";
 
-export const moduleExist = async (moduleCode, acadYear) => {
-	return getModule(moduleCode, acadYear)
-		.then(res => { return (res.data ? true : false) })
-};
-
-export const getModule = async (moduleCode, acadYear) => {
+const generateQueryUrl = (moduleCode, acadYear) => {
+	if (moduleCode === undefined) {
+		console.error("getMdule: no moduleCode to search")
+		return undefined
+	}
 
 	if (acadYear === undefined) {
 		const currentYear = new Date().getFullYear()
 		acadYear = currentYear.toString() + "/" + (currentYear + 1).toString()
 	}
 
-	const queryUrl
-		= BASE_URL
+	return BASE_URL
 		+ acadYear.replace("/", "-").toUpperCase().replace("AY", "")
 		+ "/modules/"
 		+ moduleCode.toUpperCase()
 		+ ".json"
+}
 
+export const moduleExist = async (moduleCode, acadYear) => {
+	const queryUrl = generateQueryUrl(moduleCode, acadYear) 
 	return fetch(queryUrl)
-		.catch(err => { const mute = err })
+		.then(res => { return res.ok })
+};
+
+export const getModule = async (moduleCode, acadYear) => {
+	const queryUrl = generateQueryUrl(moduleCode, acadYear) 
+	return fetch(queryUrl)
 		.then(res => res.json())
 		.then(
-			(result) => { return {data: result} },
+			(result) => { return { data: result } },
 			(err) => { return { error: err } }
 		)
-		
 };
