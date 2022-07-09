924a31d5e62dd1fc254f91dfd86140a11bdf373b
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-07-09 16:18:19 +0800

    feat(database): improved db helper
    able to get row id from 'modules' table
    able to directly pull data from api and insert into 'modules' table

--- src/hooks/Database.js
+++ src/hooks/Database.js
@@ -1,4 +1,5 @@
 import { supabase } from "../supabaseClient";
+import { pullModule, formatPreReq } from "../hooks/NUSModsAPI";
 
 export const getCourseNames = async () => {
     try {
@@ -13,6 +14,31 @@ export const getCourseNames = async () => {
 
     } catch (error) {
         console.error(error.message)
+    }
+};
+
+export const getModuleId = async (modCode, year) => {
+    try {
+        let { data, status, error } = await supabase
+            .from("modules")
+            .select("id")
+            .eq('acad_year', year)
+            .eq('code', modCode.toUpperCase())
+            .single()
+
+        if (error && status == 406) {
+            /*
+             * Error 406: multiple (or no) rows returned
+             * No row fround is expected, do nothing here is suppress it
+             */
+        } else if (data == null)
+            throw error
+
+        return data
+
+    } catch (error) {
+        console.error(error.message)
+        return null
     }
 };
 
@@ -31,6 +57,41 @@ export const getModInfo = async (modId) => {
 
     } catch (error) {
         console.error(error.message)
+    }
+};
+
+export const upsertModule = async (moduleCode, acadYear) => {
+
+    try {
+        const modData = (await pullModule(moduleCode, acadYear)).data
+
+        // Package data properly
+        const updates = {
+            code: modData.moduleCode,
+            name: modData.title,
+            description: modData.description,
+            acad_year: modData.acadYear,
+            credit: modData.moduleCredit,
+            preclusion: modData.preclusion,
+            pre_req: formatPreReq(modData.prereqTree),
+            updated_at: new Date(),
+        }
+
+        // if table has existing entry, add the id to update the correct entry
+        const existingRowId = await getModuleId(moduleCode, acadYear)
+        if (existingRowId)
+            updates.id = existingRowId.id
+
+        let { data, error } = await supabase.from('modules').upsert(updates)
+
+        if (error)
+            throw error
+
+        console.log("Database Updated:", moduleCode, acadYear)
+        return data[0].id
+
+    } catch (error) {
+        console.error(error.message);
     }
 };
 
