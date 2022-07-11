b854d7e9682ab7b37ee05ce8f4e0010b156f9b9c
LAPTOP-PE4TRUP8\Kristen <kristenchung4@gmail.com>
2022-07-11 09:34:34 +0800

    Intergrated the moduleclass to work with semester

--- src/components/SemesterSchedule.js
+++ src/components/SemesterSchedule.js
@@ -7,6 +7,7 @@ import cloneDeep from "lodash/cloneDeep";
 import withScrolling from "react-dnd-scrolling";
 
 import { graphData } from "../hooks/GraphData";
+import { ModuleSemseterStateColor } from "../constants"
 
 import "../css/SemesterSchedule.css";
 
@@ -21,6 +22,7 @@ export default function semesterTable() {
     setColumns(timeTableColumn);
   }, [timeTableMods, timeTableColumn]);
 
+  /*
   const checkPre = (draggedMod, columns, index) => {
     const modPre = mods.find((a) => a.id === draggedMod).pre;
     let status = false;
@@ -62,6 +64,141 @@ export default function semesterTable() {
       }
       return status;
     }
+  }; */
+
+  const label = (andMod, orMod)=> {
+
+    const orLabel = (orArray) => {
+      let label = "(" + orArray[0].toString();
+      for (var x = 1; x < orArray.length; x++) {
+        label = label + " or " + orArray[x];
+      }
+      return label + ")";
+    };
+
+    let msg = ""
+    if(andMod.length !==0) {
+      msg = andMod[0]
+      for(var x = 1; x < andMod.length; x++) {
+        msg = msg + " and " + andMod[x]
+      }
+      if (orMod.length !== 0){
+        msg = msg + " and "
+      } else {
+        return msg
+      }
+    }
+
+    msg = msg + orLabel(orMod[0])
+    for(var orCount = 1; orCount < orMod; orCount++) {
+      msg = msg + " and " + orLabel(orMod[orCount])
+    }
+
+    return msg
+    
+  }
+
+  const findPreqCol = (col, mod, index) => {
+    let x = false;
+    for (var colIdx = 1; colIdx < index; colIdx++) {
+      x = col[colIdx].items.some((y) => y.id === mod.toString());
+      if (x) {
+        //console.log(x)
+        return x;
+      }
+    }
+    // console.log(x)
+    return x;
+  };
+
+  const findDepCol = (col, mod) => {
+    let x = false;
+    for (var colIdx = 2; colIdx < col.length; colIdx++) {
+      x = col[colIdx].items.some((y) => y.id === mod.toString());
+      if (x) {
+        const arr = [];
+        arr[0] = colIdx;
+        arr[1] = col[colIdx].items.findIndex((y) => y.id === mod.toString());
+        return arr;
+      }
+    }
+
+    return x;
+  };
+
+  const check = (checkPreq, checkOrPreq, columns, desIndex, srcIndex, index ) => {
+    let totalPreqCount = 0;
+    for (var preqIndex = 0; preqIndex < checkPreq.length; preqIndex++) {
+      if (findPreqCol(columns, checkPreq[preqIndex], desIndex)) {
+        totalPreqCount++;
+      }
+    }
+
+    let totalOrPreqCount = 0;
+    for (var preqOrIndex = 0; preqOrIndex < checkOrPreq.length; preqOrIndex++) {
+      let orCount = 0;
+      for (
+        var orIndex = 0;
+        orIndex < checkOrPreq[preqOrIndex].length;
+        orIndex++
+      ) {
+        if (findPreqCol(columns, checkOrPreq[preqOrIndex][orIndex], desIndex)) {
+          orCount++;
+        }
+      }
+      if (orCount > 0) {
+        totalOrPreqCount++;
+      }
+    }
+
+    //toggling the color
+    if (
+      totalOrPreqCount === checkOrPreq.length &&
+      totalPreqCount === checkPreq.length
+    ) {
+      columns[srcIndex].items[index].semColor = ModuleSemseterStateColor.Normal;
+    } else {
+      columns[srcIndex].items[index].semColor = ModuleSemseterStateColor.Locked;
+      setMessage("Prequities of " + columns[srcIndex].items[index].id + " not met.\n  Requires: "
+      + label(checkPreq, checkOrPreq))
+    }
+  };
+
+  const preqCheck = (draggedMod, columns, desIndex, srcIndex, index) => {
+    const selectedMod = mods.find((a) => a.id === draggedMod);
+    const checkPreq = selectedMod.preq;
+    const checkOrPreq = selectedMod.orPreq;
+    //const checkDepMod = selectedMod.dependentMods;
+
+    //if it returns to the module table preq doesnt have to be check
+    if (desIndex === 0) {
+      columns[srcIndex].items[index].semColor = ModuleSemseterStateColor.Normal;
+      //if it does not preq then doesnt have to be check
+    } else if (checkPreq.length === 0 && checkOrPreq.length === 0) {
+      columns[srcIndex].items[index].semColor = ModuleSemseterStateColor.Normal;
+    } else {
+      check(checkPreq, checkOrPreq, columns, desIndex, srcIndex, index);
+    }
+  };
+
+  const backwardCheck = (draggedMod, columns) => {
+    const selectedMod = mods.find((a) => a.id === draggedMod);
+    const checkDepMod = selectedMod.dependentMods;
+    if (checkDepMod.length !== 0) {
+      for (var depModcount = 0; depModcount < checkDepMod.length; depModcount++ ) {
+        const depCheck = findDepCol(columns, checkDepMod[depModcount]);
+        if (depCheck !== false) {
+          const depSrc = depCheck[0];
+          const depIndex = depCheck[1];
+
+          const depMod = mods.find((a) => a.id === checkDepMod[depModcount]);
+          const depModPreq = depMod.preq;
+          const depModOrPreq = depMod.orPreq;
+
+          check(depModPreq, depModOrPreq, columns, depSrc, depSrc, depIndex);
+        }
+      }
+    }
   };
 
   const onDragEnd = (result, columns, setColumns) => {
@@ -73,10 +210,14 @@ export default function semesterTable() {
     const index = destination.droppableId;
     const modId = result.draggableId;
 
+    /*
     if (checkPre(modId, columns, parseInt(index)) === false) {
       setMessage("Prequities unmet!");
       return;
-    }
+    }*/
+
+    preqCheck( result.draggableId, columns, parseInt(destination.droppableId),  
+      parseInt(source.droppableId), source.index );
 
     const sourceColumn = columns[source.droppableId];
     const sourceItems = [...sourceColumn.items];
@@ -87,8 +228,11 @@ export default function semesterTable() {
     columnCopy[source.droppableId].items.splice(source.index, 1);
     columnCopy[destination.droppableId].items.splice(destination.index, 0, removed);
 
+    backwardCheck(result.draggableId, columnCopy);
+
     console.log(columnCopy);
     setColumns(columnCopy);
+    backwardCheck(result.draggableId, columnCopy);
   };
 
   const ScrollingComponent = withScrolling("div");
@@ -158,12 +302,12 @@ export default function semesterTable() {
                                               minHeight: "25px",
                                               backgroundColor: snapshot.isDragging
                                                 ? "#263B4A"
-                                                : "#456C86",
+                                                : item.semColor,
                                               color: "white",
                                               ...provided.draggableProps.style
                                             }}
                                           >
-                                            {item.content}
+                                            {item.label}
                                           </div>
                                         );
                                       }}
