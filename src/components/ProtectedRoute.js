2a213233276af71bf62a2243cc8b3d692bb5f9c4
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-07-13 16:35:12 +0800

    clean(ProtectedRoute): removed ununsed code

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -5,9 +5,3 @@ export function UserOnlyRoute(props) {
 	const user = supabase.auth.user();
 	return user ? <Outlet/> : <Navigate to ='/sign-up'/>
 }
-
-export function AdminOnlyRoute(props) {
-	const user = supabase.auth.user();
-	//TODO
-	return user ? <Outlet /> : <Navigate to='/' />
-}
